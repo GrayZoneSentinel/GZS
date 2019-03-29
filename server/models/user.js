3f7b44a3befdd601055353c18f3966764ce65f0f
Ernesto García Balda <grayzonesentinel@MacBook-Pro-de-Ernesto.local>
2019-03-29 20:28:30 +0100

    Generate user tokens

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 // Encryption of user passwords
 const bcrypt = require('bcrypt');
+// Generate user tokens
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email: {
@@ -60,6 +63,24 @@ userSchema.pre('save', function(next){
     }
 });
 
-const User = mongoose.model('User', userSchema);
+// Generate user token: compare password
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch)
+    });
+};
+// Generate user token: generate token
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+    // user.id + superSecretPassOfServer
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user)
+    });
+};
 
+const User = mongoose.model('User', userSchema);
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -32,6 +32,25 @@ app.post('/api/users/register', (req, res) => {
             userdata: doc
         });
     });
+});
+
+// Generate user token
+app.post('/api/users/login',(req,res)=>{
+    // Find email
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
+        // Check password
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+            // Generate Token
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
+        })
+    })
 })
 
 const port = process.env.PORT || 3002;
